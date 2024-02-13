aedae27c6724ce035b2b7b5b381377384d3325f8
TadeoQ <tadeo_mq28@hotmail.com>
2024-02-12 20:48:21 -0600

    inicio de implementacion de componentes, empezamos por el horario

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Inicio from './components/Inicio.vue';
 import Autoayuda from './components/autoayuda.vue';
 import PrimeraCita from './components/PrimeraCita.vue';
+import Horario from './components/Horario.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/PrimeraCita',
     name: 'PrimeraCita',
     component: PrimeraCita,
+  },
+  {
+    path: '/Horario',
+    name: 'Horario',
+    component: Horario,
   },
 ];
 
