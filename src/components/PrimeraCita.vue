439976ccdda22055b800fb8f0aeceb23d8f2b7c5
TadeoQ <tadeo_mq28@hotmail.com>
2024-02-13 01:24:35 -0600

    Implementacion primera cita

--- src/components/PrimeraCita.vue
+++ src/components/PrimeraCita.vue
@@ -1,3 +1,69 @@
 <template>
-    <h1>hola3</h1>
-</template>
+    <div class="fondo">
+      <div class="texto-centrado" :key="textoActual" ref="texto" v-html="textoActual">
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        listaDeTextos: [
+          "HOLA",
+          "GRACIAS POR COMUNICARTE A ACOMPAÑAMIENTO",
+          "A CONTINUACIÓN VERÁS UN CUESTIONARIO",
+          "TODA LA INFORMACIÓN ES 100% CONFIDENCIAL",
+          "CUANDO ESTÉS LISTO, PRESIONA <a href='https://www.instagram.com/__carlosg14__/' >AQUÍ</a>",
+        ],
+        textoActual: "",
+        indiceDeTexto: 0,
+      };
+    },
+    methods: {
+      cambiarTexto() {
+        if (this.indiceDeTexto < this.listaDeTextos.length) {
+          this.textoActual = this.listaDeTextos[this.indiceDeTexto];
+          this.indiceDeTexto++;
+        }
+  
+        //tiempo
+        setTimeout(this.cambiarTexto, 3000);
+      },
+    },
+    mounted() {
+      // cambio de texto
+      this.cambiarTexto();
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .fondo {
+    background-color: #ed6337;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+  }
+  
+  .texto-centrado {
+    color: #fff;
+    font-size: 24px;
+    opacity: 0;
+    animation: aparecer 1s forwards;
+    font-style: italic;
+  }
+  
+  .texto-centrado.animado {
+    animation: aparecer 1s forwards;
+  }
+  
+  @keyframes aparecer {
+    to {
+      opacity: 1;
+    }
+  }
+  
+  </style>
